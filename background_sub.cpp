03d95786c170608823d51e4ae3208f5249a2fd50
Megan Van Welie <megan.van.welie@gmail.com>
2016-10-25 11:56:59 -0400

    crop to tank and custom bckgnd subtraction

--- background_sub.cpp
+++ background_sub.cpp
@@ -30,6 +30,45 @@
 using namespace cv;
 using namespace std;
 
+Rect rectFromMask(Mat& mask) {
+    int l = -1;
+    int r = -1;
+    int t = -1;
+    int b = -1;
+    int row, col;
+    for ( row = 0; row < mask.rows; ++row ) {
+        for ( col = 0; col < mask.cols; ++col ) {
+            if (mask.at<uchar>(row, col) == 255) {
+                if ( l == -1 ) { l = col; }
+                if ( col > r ) { r = col; }
+                if ( t == -1 ) { t = row; }
+                if ( row > b ) { b = row; }
+            }
+        }
+    }
+    int w = r - l;
+    int h = b - t;
+    Rect roi(l, t, w, h);
+    return roi;
+}
+
+void meanOfHistory(vector<Mat> history, Mat& dst) {
+    int rows = history[0].rows;
+    int cols = history[0].cols;
+    vector<uchar> merging;
+    int i, row, col;
+    for ( row = 0; row < rows; ++row ) {
+        for ( col = 0; col < cols; ++col ) {
+            merging.clear();
+            for ( i = 0; i < history.size(); ++i ) {
+                merging.push_back(history[i].at<uchar>(row, col));
+            }
+            double avg = mean(merging)[0];
+            dst.at<uchar>(row, col) = avg;
+        }
+    }
+}
+
 vector<string> filesInFolder(string& dirpath)
 {
 	glob_t glob_result;
@@ -109,11 +148,8 @@ int main(int argc, char** argv)
 	Ptr<BackgroundSubtractor> pMOG2; //MOG2 Background subtractor
 
 	pMOG2 = new BackgroundSubtractorMOG2(100,10,false);
-	string dirpath = "data/eels/*";
+	string dirpath = "../../hw4/data/eels2/*";
 	vector<string> img_files = filesInFolder(dirpath);
-	namedWindow("window",WINDOW_AUTOSIZE);
-	namedWindow("final",WINDOW_AUTOSIZE);
-	namedWindow("grey",WINDOW_AUTOSIZE);
 
 	if ( img_files.size() == 0 ) {
 			cout << "Data files in " << dirpath << " were not found!" <<  endl;
@@ -122,41 +158,41 @@ int main(int argc, char** argv)
 
     Mat tank_mask;
     createMatchedMask( img_files[0],
-                       "data/background_template.png", tank_mask);
+                       "../../hw4/data/background_template.png", tank_mask);
     if ( tank_mask.empty() ) {
         cout << "ERROR: Failed to create tank mask" << endl;
         return -1;
     }
-    resize(tank_mask, tank_mask, Size(), 0.5, 0.5, INTER_LANCZOS4);
+    //resize(tank_mask, tank_mask, Size(), 0.5, 0.5, INTER_LANCZOS4);
+    Rect tank = rectFromMask(tank_mask);
 
-	Mat orig, smaller, mask_flip, mask_color, mask_convert, grey, thresh1, thresh2;
-	Mat thresh1_color,thresh2_color, thresh1_convert, thresh2_convert, fgMaskMOG2, sElem, eroded, dilated;
-	Mat segmented, old, diff, grey1;
-	bool f = false;
+    Mat eroded, dilated;
+    Mat sElem = getStructuringElement(MORPH_RECT,Size(3,3),Point(0,0));
+
+    vector<Mat> history;
+    Mat orig, grey, prev, output, greyOut, outOut;
+    Mat buffer;
+    Mat display;
+
 	for ( int i = 0; i < img_files.size(); ++i ) {
-
 		orig = imread(img_files[i],CV_LOAD_IMAGE_COLOR);
 		if(!orig.data )
 		{
 			cout << "No image data" <<  endl;
 			return -1;
 		}
+		//resize(orig, orig, Size(), 0.5, 0.5, INTER_LANCZOS4);
 
-		resize(orig, orig, Size(), 0.5, 0.5,INTER_LANCZOS4);
-        orig.copyTo(smaller, tank_mask);
-		if(!f)
-		{
-			old = smaller;
-			f = true;
-			continue;
-		}
-		cvtColor(smaller,grey1,CV_BGR2GRAY);
+        // isolate tank area
+        orig.copyTo(buffer, tank_mask);
+        orig = buffer;
+
+        // create grayscale image
+		cvtColor(orig, grey, CV_BGR2GRAY);
+
+        /*
 		Scalar a = mean(grey1);
-
 		grey = grey1 - a;
-
-
-		imshow("grey",grey);
 		Mat equal;
 		//equalizeHist(grey,equal);
 		threshold(grey,thresh1,255, 255, THRESH_BINARY);
@@ -165,8 +201,6 @@ int main(int argc, char** argv)
 		cvtColor(thresh2, thresh2_color,CV_GRAY2BGR );
 		thresh1_color.convertTo(thresh1_convert, CV_8UC3);
 		thresh2_color.convertTo(thresh2_convert, CV_8UC3);
-
-
 
 		pMOG2->operator()(grey, fgMaskMOG2, .0025);
 		bitwise_not(fgMaskMOG2, mask_flip);
@@ -177,18 +211,48 @@ int main(int argc, char** argv)
 		segmented = ((smaller - thresh1_convert) - thresh2_convert) - mask_convert;
 		old = smaller;
 
+        output = fgMaskMOG2;
+
 		imshow("window",smaller);
 
-
-		sElem = getStructuringElement(MORPH_RECT,Size(3,3),Point(0,0));
 		erode(fgMaskMOG2,eroded,sElem);
 		dilate(eroded,dilated,sElem);
-		imshow("final",dilated);
+        output = dilated;
+        */
 
-		if (waitKey(30) == 27) {
-			break;
-		}
+        if ( !prev.empty() ) {
+            // motion detect by subtracting previous image
+            // (may be more than 1 step back)
+            subtract(prev, grey, output);
+            threshold(output, output, 20, 255, 0);
 
+            display = Mat(tank.height*2, tank.width, orig.type());
+
+            /*
+            buffer = orig(tank);
+            buffer.copyTo(display(Rect(0, 0, buffer.cols, buffer.rows)));
+            */
+
+            cvtColor(grey(tank), buffer, CV_GRAY2BGR);
+            buffer.copyTo(display(Rect(0, 0, buffer.cols, buffer.rows)));
+
+            cvtColor(output(tank), buffer, CV_GRAY2BGR);
+            buffer.copyTo(display(Rect(0, buffer.rows, buffer.cols, buffer.rows)));
+
+            imshow("Output", display);
+
+            if (waitKey(30) == 27) {
+                break;
+            }
+        }
+
+        history.push_back(grey.clone());
+        if ( history.size() > 10 ) {
+            history.erase(history.begin());
+            //prev = history[0];
+            prev = Mat::zeros(grey.size(), grey.type());
+            meanOfHistory(history, prev);
+        }
 
 	}
 	waitKey(0);
